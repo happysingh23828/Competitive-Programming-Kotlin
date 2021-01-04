697fcc76c3277e829e474b6fb95cb405cc68ae92
Happy Singh <happysingh23828@gmail.com>
2021-01-05 05:27:13 +0530

    LeetCode1480 Solved

---
+++ leetcode/src/easy/LeetCodeProblem1480.kt
@@ -0,0 +1,18 @@
+package easy
+class Solution1480 {
+
+    fun runningSum(nums: IntArray): IntArray {
+
+        // if array has only one element
+        if(nums.size == 1) {
+            return nums
+        }
+
+
+        for (i in 1..nums.size-1) {
+            nums[i] += nums[i-1]
+        }
+
+        return nums
+    }
+}
