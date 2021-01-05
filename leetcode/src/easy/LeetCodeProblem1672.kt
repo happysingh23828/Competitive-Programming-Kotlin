c54af0bd543481c49ef47b7b32a0eacdf7d433b1
Happy Singh <happysingh23828@gmail.com>
2021-01-05 06:12:01 +0530

    LeetCode1672 Solved.

---
+++ leetcode/src/easy/LeetCodeProblem1672.kt
@@ -0,0 +1,30 @@
+package easy
+
+class Solution {
+
+    fun maximumWealth(accounts: Array<IntArray>): Int {
+
+        var maximumWealth = 0
+
+        accounts.forEach { eachRow ->
+            val sumOfEachRow = getSumOfArray(eachRow)
+
+            if (sumOfEachRow > maximumWealth) {
+                maximumWealth = sumOfEachRow
+            }
+        }
+
+        return maximumWealth
+    }
+
+    fun getSumOfArray(nums: IntArray): Int {
+
+        var totalSum = 0
+
+        for (i in nums.indices) {
+            totalSum += nums[i]
+        }
+
+        return totalSum
+    }
+}
