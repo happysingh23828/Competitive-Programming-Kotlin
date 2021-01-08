9949629d0dbca9ab76ec0c9c5befac81ae89aa8d
Happy Singh <happysingh23828@gmail.com>
2021-01-08 09:09:47 +0530

    LeetCode11 Solved.

---
+++ leetcode/src/medium/LeetCodeProblem11.kt
@@ -0,0 +1,44 @@
+package medium
+
+// Not able to solve :-(
+class Solution11WithBruteForce {
+
+    fun maxArea(height: IntArray): Int {
+
+        var maximumArea = 0
+
+        for (i in 0 until height.size) {
+
+            for (j in i + 1 until height.size) {
+
+                var areaBetweenLines = Math.min(height[i], height[j]).times(j - i)
+                maximumArea = Math.max(areaBetweenLines, maximumArea)
+
+            }
+        }
+
+        return maximumArea
+    }
+}
+
+class Solution11WithTwoPointer {
+
+    fun maxArea(height: IntArray): Int {
+
+        var maximumArea = 0
+        var left = 0
+        var right = height.size - 1
+
+        while (left < right) {
+            maximumArea = Math.max(maximumArea, Math.min(height[left], height[right]) * (right - left));
+
+            if (height[left] < height[right]) {
+                left++
+            } else {
+                right--
+            }
+        }
+
+        return maximumArea
+    }
+}
