d8b4c3d1a7e982a51f9bf060e29a8c3df9b5bd1a
Happy Singh <happysingh23828@gmail.com>
2021-01-10 01:02:01 +0530

    LeetCode1346 Solved.

---
+++ leetcode/src/easy/LeetCodeProblem1346.kt
@@ -0,0 +1,19 @@
+package easy
+
+class Solution1346 {
+    fun checkIfExist(arr: IntArray): Boolean {
+
+
+        for(i in arr.indices) {
+
+            for(j in i+1 until arr.size) {
+
+                if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+}
