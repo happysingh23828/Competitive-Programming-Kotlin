c6e734c12721b311a8b24e4783b6e692301e937e
Happy Singh <happysingh23828@gmail.com>
2021-01-09 21:30:05 +0530

    LeetCode26 Solved.

---
+++ leetcode/src/medium/LeetCodeProblem26.kt
@@ -0,0 +1,24 @@
+package medium
+
+// [Problem] https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution26 {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size == 1)
+            return 1
+
+        var updatedIndex = 1
+        for (i in nums.indices) {
+
+            if (i + 1 == nums.size)
+                return updatedIndex
+
+            if (nums[i] != nums[i + 1]) {
+                nums[updatedIndex] = nums[i + 1]
+                updatedIndex += 1
+            }
+
+        }
+
+        return updatedIndex
+    }
+}
